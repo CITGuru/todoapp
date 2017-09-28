f7cb0db8eaae96c868e01e9f30d0a45c3d5167f8
Oyetoke Toby <Oyetoke Toby>
2017-09-28 06:22:08 +0100

    Second commit

---
+++ todolist/models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Category(models.Model): # The Category table name that inherits models.Model
+	name = models.CharField(max_length=100) #Like a varchar
+
+	def __str__(self):
+		return self.name #name to be shown when called
+
+class TodoList(models.Model): #Todolist able name that inherits models.Model
+	title = models.CharField(max_length=250) # a varchar
+	content = models.TextField(blank=True) # a text field 
+	created = models.DateField(default=timezone.now().strftime("%m %d, %Y")) # a date
+	due_date = models.DateField(default=timezone.now().strftime("%m %d, %Y")) # a date
+	category = models.ForeignKey(Category, default="general") # a foreignkey
+
+	class Meta:
+		ordering = ["-created"] #ordering by the created field
+
+	def __str__(self):
+		return self.title #name to be shown when called
---
+++ todolist/views.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render,redirect
+from .models import TodoList, Category
+import datetime
+# Create your views here.
+
+def index(request): #the index view
+	todos = TodoList.objects.all() #quering all todos with the object manager
+	categories = Category.objects.all() #getting all categories with object manager
+	if request.method == "POST": #checking if the request method is a POST
+		if "taskAdd" in request.POST: #checking if there is a request to add a todo
+			title = request.POST["description"] #title
+			date = str(request.POST["date"]) #date
+			catgory = request.POST["category_select"] #category
+			content = title + " -- " + date + " " + catgory #content
+			Todo = TodoList(title=title, content=content, due_date=datetime.datetime.date(datetime.datetime(int(date[:4:]), int(date[5:7:]), int(date[8:10:]))), category=Category.objects.get(name=category))
+			Todo.save() #saving the todo 
+			return redirect("/") #reloading the page
+		
+		if "taskDelete" in request.POST: #checking if there is a request to delete a todo
+			checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+			for todo_id in checkedlist:
+				todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
+				todo.delete() #deleting todo
+
+	return render(request, "add-todo.html", {"todos": todos, "categories":categories})
