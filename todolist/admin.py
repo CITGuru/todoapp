404be2675ab4331993641cda965997db55bb5fd4
Alti <shrutiambre225@gmail.com>
2019-04-02 21:21:50 +0530

    Resolved whitenoise and missing 1 required positional argument: 'on_delete' issues

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
-
-# Register your models here.
 from . import models
 
+# Register your models here.
+
 class TodoListAdmin(admin.ModelAdmin):
-	list_display = ("title", "created", "due_date")
+    list_display = ("title",  "created", "due_date")
 
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ("name",)
+    list_display = ("name",)
 
 admin.site.register(models.TodoList, TodoListAdmin)
 admin.site.register(models.Category, CategoryAdmin)
